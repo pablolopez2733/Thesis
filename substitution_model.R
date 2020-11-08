1d2d9ceeb8074982878d359bad0bce735bf6c413
Pablo López Landeros <pablo.lopez.landeros@hotmail.com>
2020-11-07 18:56:21 -0600

    Changed file names

--- substitution_model.R
+++ substitution_model.R
@@ -9,22 +9,39 @@ library(readr)
 #-------------------------------------------------------------------------------
 
 #Read data:
-lineup_pm <- read_csv("Data/lineup_stats.csv")
+subs <- read_csv("https://github.com/pablolopez2733/Thesis/blob/main/Data/subs_1415.csv?raw=true")
+names(subs)[names(subs) == 'slugTeamPlayer'] <- 'Team'
 
-foo <- lineup_pm %>%
-  group_by(lineup,slugTeam) %>%
-  summarise(seasonPM = sum(netScoreTeam),
-            seasonSecs = sum(totalTime)) %>%
-  ungroup() %>%
-  arrange(-seasonPM) %>%
-  mutate(seasonMin = paste0(floor(seasonSecs / 60), ":", str_pad(round(seasonSecs %% 60, 0), side = "left", width = 2, pad = 0))) %>%
-  select(seasonSecs,seasonPM,slugTeam,lineup)
 
-prueba2018<- lineup_stats %>%
-  group_by(lineup) %>%
-  summarise(seasonPM = sum(netScoreTeam),
-            seasonSecs = sum(totalTime)) %>%
-  ungroup() %>%
-  arrange(-seasonPM) %>%
-  mutate(seasonMin = paste0(floor(seasonSecs / 60), ":", str_pad(round(seasonSecs %% 60, 0), side = "left", width = 2, pad = 0))) %>%
-  select(-seasonSecs)
+#Returns a list
+subs_by_team <- subs %>% 
+  group_by(Team)
+subs_by_team <- group_split(subs_by_team)
+
+#Let´s just play with DAllas----------------------------------------------------
+dallas <- as.data.frame(subs_by_team[[7]]) #get dallas subs
+
+
+#New approach, we first bind all lineups and then generate ids
+all_lineups <- data.frame("lineups" = c(unique(dallas$lineupAfter),unique(dallas$lineupBefore))) 
+dict <- data.frame("Units" = unique(all_lineups$lineups))
+dict$id <- 1:nrow(dict)
+
+test_join <- dallas %>% 
+  left_join(dict, by = c("lineupBefore" = "Units")) %>% 
+  rename(id_Before = id)
+
+test_join <- test_join %>% 
+  left_join(dict , by = c("lineupAfter" = "Units")) %>% 
+  rename(id_After = id)
+#-----------------------------------------------------------------------------
+dallas_pm$id <- 1:nrow(dallas_pm)
+dallas_subs <- subs %>% filter(Team == "DAL")
+
+test_join <- dallas_subs %>% 
+  left_join(dallas_pm, by = c("lineupBefore" = "lineup")) %>% 
+  rename(id_Before = id)
+
+test_join <- test_join %>% 
+  left_join(dallas_pm, by = c("lineupAfter" = "lineup")) %>% 
+  rename(id_After = id)
