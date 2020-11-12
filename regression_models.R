5dc446b70dd328859890d670548c0cc04a2b8fd5
Pablo López Landeros <pablo.lopez.landeros@hotmail.com>
2020-11-11 21:35:38 -0600

    Documented code | x_matrix works

--- regression_models.R
+++ regression_models.R
@@ -1,15 +1,17 @@
 #------------------SCRIPT FOR REGRESSION MODELS-------------------------------
-#Libraries:
+# Libraries--------------------------------------------------------------------
 library(tidyverse)
 library(dplyr)
 library(readr)
-#-----------------------------------------------------------------------------
 
-#Read Data:
+
+# Read Data--------------------------------------------------------------------
 lineup_stats <- read_csv("https://github.com/pablolopez2733/Thesis/blob/main/Data/lineup_stats.csv?raw=true")
 grouped_pm <- read.csv("https://github.com/pablolopez2733/Thesis/blob/main/Data/grouped_lineup_stats.csv?raw=true")
 
-X_matrix <- function(team){
+# Function x_matrix------------------------------------------------------------
+# 'Returns the N_i x 82 matrix for the team given as an argument.
+x_matrix <- function(team){
   team_series <- lineup_stats %>% 
     filter(slugTeam == team) %>% 
     group_by(idGame,lineup) %>% 
@@ -19,21 +21,26 @@ X_matrix <- function(team){
       
     ) %>% 
     left_join(grouped_pm, by = c("lineup" = "lineup"))
-    
-  #this should output a tibble where we have the lineups used in every game
-  #their time, and pm. Seems to work
+  # This should output a tibble where we have the lineups used in every game
+  # their time, and pm.
+  
+  # Create matrix
   N_i <- nrow(as.data.frame(unique(team_series$l_id)))
-  X <- matrix(0L, nrow = 82, ncol = N_i) #create matrix
-  #rename matrix so that columns and rows are lineup_ids:
+  X <- matrix(0L, nrow = 82, ncol = N_i)
+  
+  #Rename matrix so that columns and rows are lineup_ids:
   colnames(X) <- unique(team_series$l_id) 
   rownames(X) <- unique(team_series$idGame)
   
   
   lineups <- colnames(X)
   games <- rownames(X)
+  # Iterate through all the possible lineups and games
   for(l in lineups){
     for (g in games) {
       dato <- team_series[which(team_series$idGame == as.double(g) & team_series$l_id == as.double(l)),]$time_played
+      # Because a lineup may not have appeared in a game, we need to account for
+      # length zero values
       if(length(dato) == 0)
         X[g,l] <- 0
       else
@@ -46,4 +53,14 @@ X_matrix <- function(team){
   
 }
 
-test_clevx <- X_matrix("CLE")
+# Function Y_Vector-------------------------------------------------------------
+# 'Returns the 82 x 1 vector of point differential for every game
+# 'of a given team. 
+y_vector <- function(team){
+  
+}
+
+
+#Test---------------------------------------------------------------------------
+# Test with Cleveland Cavaliers:
+test_clevx <- x_matrix("CLE")
--- substitution_model.R
+++ substitution_model.R
@@ -2,51 +2,49 @@
 #subtitution Stochastic Model
 #-------------------------------------------------------------------------------
 
-#--------------------LIBRARIES--------------------------------------------------
+# Libraries--------------------------------------------------------------------
 library(tidyverse)
 library(dplyr)
 library(readr)
-#-------------------------------------------------------------------------------
-#Read Data:
+
+##Read Data---------------------------------------------------------------------
 lineup_stats <- read_csv("https://github.com/pablolopez2733/Thesis/blob/main/Data/lineup_stats.csv?raw=true")
 grouped_pm <- read.csv("https://github.com/pablolopez2733/Thesis/blob/main/Data/grouped_lineup_stats.csv?raw=true")
 
-#create id for every single 5-man unit:
-grouped_pm$l_id <- 1:nrow(grouped_pm)
-#calculate minutes for each lineup:
-grouped_pm$Season_mins <- floor(grouped_pm$seasonSecs / 60)
+
 #Now every lineup for every team has a unique_id
 #Thus, grouped_pm will act as a dictionary
-
 teams <- unique(grouped_pm$slugTeam)
 
-
-#-------FUNCTION FOR CALCULATING TRANSITION MATRIX FOR GIVEN TEAM------------------
+# Function transition_matrix----------------------------------------------------
+# 'Function for calculating the N_i x N_i matrix for a given team
 
 transition_matrix <- function(team){
-  team_df <- lineup_stats %>% filter(slugTeam == team) #get team df (as sort of time series)
+  # Get team df (as sort of time series)
+  team_df <- lineup_stats %>% filter(slugTeam == team) 
   team_df <- team_df %>% 
-    left_join(grouped_pm, by = c("lineup" = "lineup")) #add l_id for each lineup
+    # Add l_id for each lineup 
+    left_join(grouped_pm, by = c("lineup" = "lineup")) 
   
   dimension <- nrow(as.data.frame(unique(team_df$l_id)))
-  
-  tm <- matrix(0L, nrow = dimension, ncol = dimension) #create matrix
-  #rename matrix so that columns and rows are lineup_ids:
+  # Create matrix
+  tm <- matrix(0L, nrow = dimension, ncol = dimension) 
+  # Rename matrix so that columns and rows are lineup_ids:
   colnames(tm) <- unique(team_df$l_id) 
   rownames(tm) <- unique(team_df$l_id)
   
-  #create columns for the transitions
+  # Create columns for the transitions
   team_df<- team_df %>% 
     mutate(
       t_1 = lag(l_id),
       t_2 = l_id
     )
   
-  #iterate through df to fill the matrix:
+  # Iterate through df to fill the matrix:
   for (i in 2:nrow(team_df)) {
     tm[toString(team_df$t_1[i]),toString(team_df$t_2[i])] = tm[toString(team_df$t_1[i]),toString(team_df$t_2[i])] + 1
   }
-  
+  # Divide by time spent in origin state (to get MLE)
   N <- tm
   nombres <- rownames(tm)
   for(i in nombres){
@@ -58,9 +56,9 @@ transition_matrix <- function(team){
   
 }
 
-#test it:
+# Test -------------------------------------------------------------------------
 test_cleveland <- transition_matrix("CLE")
 
-#------------------------------NOTES--------------------------------------
-#Should we try with minutes instead and filter out some?
-#because: absorbent states and working with bigger numbers
+#NOTES--------------------------------------------------------------------------
+# Should we try with minutes instead and filter out some?
+# because: absorbent states and working with bigger numbers.
